4b552d4bc3b1c6db4377e01b8c810c23dbd1c90a
r.hata <uwfrfr@gmail.com>
2020-04-18 02:53:41 +0900

    #3 message-wrapper add

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,5 @@
 class PagesController < ApplicationController
-
+  before_action :authenticate_user!
   def home
   end
-
-  def psychology
-  end
-
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only:[:new, :create, :destroy]
+  before_action :authenticate_user!#, only:[:new, :create, :destroy]
   before_action :set_post, only: [:show, :destroy]
 
   def index
