7e1660d07a15000c59067167d7fa6ddf2b996513
r.hata <uwfrfr@gmail.com>
2020-06-07 17:32:07 +0900

    timezone edit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
+    if user_signed_in?
+      redirect_to user_passports_path(current_user)
+    else
+      redirect_to root_path
+    end
   end
 end
