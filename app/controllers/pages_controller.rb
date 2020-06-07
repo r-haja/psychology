3b674022664db1236a0112677998534ef43fb903
r.hata <uwfrfr@gmail.com>
2020-06-07 21:52:54 +0900

    page edit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,6 @@ class PagesController < ApplicationController
   def home
     if user_signed_in?
       redirect_to user_passports_path(current_user)
-    else
-      redirect_to root_path
     end
   end
 end
