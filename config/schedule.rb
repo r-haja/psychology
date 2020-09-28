76750a4cd120350ab9042858cde5f8d0f47f690c
r.hata <uwfrfr@gmail.com>
2020-09-28 20:16:37 +0900

    バッチ処理修正

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,10 +3,13 @@
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 require File.expand_path(File.dirname(__FILE__) + "/environment")
-set :environment, "development"
 set :output, { :error => "log/cron_error.log" }
 #set :path, "appへのpath"
 
 every 1.day, at: '11:50 pm' do
-  runner ""
+  runner "Notification.new().notification_create(@schedule.passport)"
+end
+
+every 1.minute do
+  command "echo 'mossmossmossmossmossmoss'"
 end
